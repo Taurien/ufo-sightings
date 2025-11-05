ff934e241b508608bbb19b19f09d14d8221e6123
Taurien <michel.a.c474@gmail.com>
2025-11-05 16:26:43 -0500

    Update Astro configuration Node.js

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   output: "server",
-
   integrations: [react()],
 
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
